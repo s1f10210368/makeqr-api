10592e50bf328ef0d2118a0887e9decc18f9512e
s1f10210368 <s1f102103684@iniad.org>
2023-11-01 16:11:34 +0900

    mojibake

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,18 @@ use serde_json::Value;
 extern crate qrcode;
 use qrcode::QrCode;
 use qrcode::render::svg;
+/*use qr2term::print_qr;*/
 
-#[tokio::main]
+#[tokio::main]  // これをつけると非同期関数が使える
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // OpenAI APIの認証キーとエンドポイントを設定
-    let api_key = "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";
+    let api_key = "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";
     let endpoint = "https://api.openai.com/v1/chat/completions";
 
     // APIリクエストのボディをJSON形式で設定
     let request_body = json!({
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "system", "content": "Unityについて教えてください"}]
+        "messages": [{"role": "system", "content": "こんにちは!"}]
     });
 
     // HTTPクライアントのインスタンスを作成
@@ -38,8 +39,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Some(content) = response_json["choices"][0]["message"]["content"].as_str() {
         println!("Content: {}", content);
 
+        let utf8_content = content.as_bytes();
+
         // QRコードを生成
-        let code = QrCode::new(content).unwrap();
+        let code = QrCode::new(utf8_content).unwrap();
 
         // QRコードをSVG形式でレンダリング
         let image = code.render()
@@ -47,13 +50,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .light_color(svg::Color("#ffffff"))
             .build();
 
+        // ターミナルに出力するときは以下を用いる
+        /*let qr_term = code.render()
+            .dark_color('　')
+            .light_color('■')
+            .build();
+        */
+
         // SVG形式のQRコードをファイルに保存
+        //std::fs::write("qrcode.svg", image).expect("Unable to write QR code image");
+        // SVG形式のQRコードをファイルに保存（バイナリエンコーディング）
         std::fs::write("qrcode.svg", &image).expect("Unable to write QR code image");
+
+
+        // ターミナルに表示
+        /*println!("{}", qr_term);*/
     } else {
         println!("Content not found in the response.");
     }
 
-    Ok()
+    Ok(())
 }
 
 
