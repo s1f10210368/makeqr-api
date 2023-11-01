09a86a4ca77d838620e600ed46cfdec7afae880d
s1f10210368 <s1f102103684@iniad.org>
2023-11-01 14:54:58 +0900

    comment

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,21 @@ use qrcode::QrCode;
 use qrcode::render::svg;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> { 
-    let api_key = "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";
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // OpenAI APIの認証キーとエンドポイントを設定
+    let api_key = "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";
     let endpoint = "https://api.openai.com/v1/chat/completions";
 
+    // APIリクエストのボディをJSON形式で設定
     let request_body = json!({
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "system", "content": "こんにちは！"}]
+        "messages": [{"role": "system", "content": "Unityについて教えてください"}]
     });
 
+    // HTTPクライアントのインスタンスを作成
     let client = Client::new();
+
+    // APIリクエストを送信し、レスポンスを取得
     let response = client
         .post(endpoint)
         .header("Authorization", format!("Bearer {}", api_key))
@@ -23,28 +28,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .send()
         .await?;
 
+    // レスポンスのテキストを取得
     let result = response.text().await?;
 
+    // レスポンスをJSONにパース
     let response_json: Value = serde_json::from_str(&result)?;
 
-    // contentを取得
+    // レスポンスから"content"を取得
     if let Some(content) = response_json["choices"][0]["message"]["content"].as_str() {
         println!("Content: {}", content);
 
+        // QRコードを生成
         let code = QrCode::new(content).unwrap();
 
+        // QRコードをSVG形式でレンダリング
         let image = code.render()
-        .dark_color(svg::Color("#000000"))
-        .light_color(svg::Color("#ffffff"))
-        .build();
+            .dark_color(svg::Color("#000000"))
+            .light_color(svg::Color("#ffffff"))
+            .build();
 
+        // SVG形式のQRコードをファイルに保存
         std::fs::write("qrcode.svg", &image).expect("Unable to write QR code image");
     } else {
         println!("Content not found in the response.");
     }
 
-    Ok(())
+    Ok()
 }
+
 
 
 
