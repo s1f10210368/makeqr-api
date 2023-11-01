e383a0e1ba3adb730eab2864bd1e2602449fc2aa
s1f10210368 <s1f102103684@iniad.org>
2023-11-01 13:27:21 +0900

    response-complete

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use reqwest::Client;
 use serde_json::json;
+use serde_json::Value;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> { 
-    let api_key = "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";
+    let api_key = "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";
     let endpoint = "https://api.openai.com/v1/chat/completions";
 
     let request_body = json!({
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": "who are you?"}]
+        "messages": [{"role": "system", "content": "こんにちは！"}]
     });
 
     let client = Client::new();
@@ -19,8 +21,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     let result = response.text().await?;
-    
-    println!("Response: {}", result);
+
+    let response_json: Value = serde_json::from_str(&result)?;
+
+    // contentを取得
+    if let Some(content) = response_json["choices"][0]["message"]["content"].as_str() {
+        println!("Content: {}", content);
+    } else {
+        println!("Content not found in the response.");
+    }
 
     Ok(())
 }
